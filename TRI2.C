c561cc248057844b2963695971f28be32ee543c6
edzeldeguzman <72650887+edzeldeguzman@users.noreply.github.com>
2020-10-17 23:15:45 +0800

    Add files via upload
    Sir here's mine

--- TRI2.C
+++ TRI2.C
@@ -1,20 +1,12 @@
 #include<stdio.h>
-
 void main()
-{
-  int i,j, k;
-  clrscr();
-
-  for(i = 0; i < 24; i++);
-  {
-     for(j = i; j >= 0; j--);
-     {
-	printf(" ");
-     }
-     for(k = 0; k <= i; k++)
-       printf("*");
-     printf("\n");
-  }
-
-  getch();
+{int i, j, k; clrscr();
+for(i = 0; i < 24; i++)
+{for(j = 24; j >= i; j--)
+{printf(" ");}
+for(k = 0; k <= i; k++)
+printf("*");
+printf("\n");
+}
+getch();
 }
